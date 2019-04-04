6d6787ad9ac54cec09d618d5c88fd233f969590a
Gerald Büchler <dev@genomast.de>
2019-04-04 20:50:27 +0200

    vuemastery c03 tut (conditional-rendering)

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: "Boots",
+        product: "Socks",
         description: "A pair of warm, fuzzy socks",
         image: "./image/vmSocks-green-onWhite.jpg",
-        myurl: "https://github.com"
+        myurl: "https://github.com",
+        inventory: 100,
+        inStock: false
     }
 })
