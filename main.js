56f6e9ebfd3d51dbf74523d234693b33605f46c8
Gerald Büchler <dev@genomast.de>
2019-04-16 22:13:59 +0200

    vuemastery c05 tut (event handling)

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        description: "A pair of warm, fuzzy socks",
-        image: "./image/vmSocks-green-onWhite.jpg",
-        myurl: "https://github.com",
-        inventory: 100,
-        inStock: false,
-        onSale: true,
-        details: ["80% cotton","20% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantID: 2234,
-                variantColor: "green"
-            },
-            {
-                variantID: 2235,
-                variantColor: "blue"
-            }
-        ],
-        sizeAvail: ["37-39", "40-42", "43-45"]        
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks",
+    image: "./image/vmSocks-green-onWhite.jpg",
+    myurl: "https://github.com",
+    inventory: 100,
+    inStock: false,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantID: 2234,
+        variantColor: "green",
+        variantImage: "./image/vmSocks-green-onWhite.jpg"
+      },
+      {
+        variantID: 2235,
+        variantColor: "blue",
+        variantImage: "./image/vmSocks-blue-onWhite.jpg"
+      }
+    ],
+    cart: 0,
+    sizeAvail: ["37-39", "40-42", "43-45"]
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+        this.image = variantImage
     }
-})
+  }
+});
